ee36495a27a4998bfa36203f09cafba2888b1a32
Michael McCartney <m.a.mccartney@gmail.com>
2016-05-10 13:14:20 -0500

    Refactor middleware

---
+++ 49-server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+var middleware = require('./49-middleware.js');
+
+app.use(middleware.logger);
+
+app.get('/about', middleware.requireAuthentication, function (req, res) {
+  res.send('About Us');
+});
+
+// console.log(__dirname);
+app.use(express.static(__dirname + '/public'));
+
+app.listen(PORT, function() {
+  console.log('Express server started on port ' + PORT);
+});
